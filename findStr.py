572dac6b28c5c55af3cef50ef14e4ffe6ec9ec3d
theingz <934320370@qq.com>
2021-01-20 15:45:27 +0800

    添加一个查找序列的小工具

--- findStr.py
+++ findStr.py
@@ -1,20 +1,22 @@
-from tools import *
+from bin.tools import *
 
 
 def find(path, findStr):
     a = 0
-    print(list_name(path))
+    # print(list_name(path))
     for names in list_name(path):
         pathName = path + names
         if findStr in open(pathName).read():
             a = a + 1
-            # print(pathName)
-        else:
             print(pathName)
+        else:
+            pass
+            # print(pathName)
     print(a)
 
 
 if __name__ == '__main__':
-    path = ".\\src\\sequence\\"
-    findStr = "Amborella_trichopoda"
+    path = ".\\bulid\\megax\\aligned2\\"
+    findStr = "-------------------------------------------ATGGAAAAA"
+
     find(path, findStr)
