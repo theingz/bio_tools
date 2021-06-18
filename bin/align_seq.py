fabcd14976b3fde905e90c9b92fa608eaafd4928
theingz <934320370@qq.com>
2021-06-18 19:26:33 +0800

    增加全局锁，限制线程数

--- bin/align_seq.py
+++ bin/align_seq.py
@@ -1,5 +1,5 @@
 import time
-from threading import Thread
+import threading
 from bin.tools import *
 
 
@@ -7,11 +7,14 @@ def align(names):
     """
     调取核心比对序列
     """
+    lock.acquire()
     readPath = "..\\bulid\\class\\use\\"
     outPath = "..\\bulid\\megax\\aligned\\"
     clustal_align_pro = "..\\mega_mao\\clustal_align_protein.mao"
     # clustal_align_pro = ".\\mega_mao\\clustal_align_nucleotide.mao"
     os.system("megacc -a {} -d {} -f Fasta -o {}".format(clustal_align_pro, readPath + names, outPath + names))
+    lock.release()  # 释放锁
+    threadmax.release()  # 释放信号量，可用信号量加一
 
 
 if __name__ == '__main__':
@@ -22,8 +25,11 @@ if __name__ == '__main__':
     threads = []
     readPath = "..\\bulid\\class\\use\\"
     thrNum = len(list_name(readPath))
+    threadmax = threading.BoundedSemaphore(8)  # 限制线程的最大数量为4个
+    lock = threading.Lock()  # 将锁内的代码串行化
     for i in range(thrNum):
-        t = Thread(target=align, args=(list_name(readPath)[i],))
+        threadmax.acquire()  # 增加信号量，可用信号量减一
+        t = threading.Thread(target=align, args=(list_name(readPath)[i],))
         t.start()
         threads.append(t)
     for t in threads:
