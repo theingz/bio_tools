74a40321e201334036bce7ae9588fc3b77fb41fb
theingz <934320370@qq.com>
2021-07-26 13:47:07 +0800

    更改默认线程数为16

--- bin/align_seq.py
+++ bin/align_seq.py
@@ -1,13 +1,13 @@
 import time
 import threading
-from bin.tools import *
+from tools import *
 
 
 def align(names):
     """
     调取核心比对序列
     """
-    lock.acquire()
+    # lock.acquire()
     clustal_align_pro = "..\\mega_mao\\clustal_align_protein.mao"
     # clustal_align_pro = ".\\mega_mao\\clustal_align_nucleotide.mao"
     os.system("megacc -a {} -d {} -f Fasta -o {}".format(clustal_align_pro, readPath + names, outPath + names))
@@ -19,12 +19,12 @@ if __name__ == '__main__':
     """
     实现多线程比对序列
     """
-    readPath = "..\\bulid\\class3\\use\\"
-    outPath = "..\\bulid\\megax\\align3\\"
+    readPath = "..\\bulid\\class\\use\\"
+    outPath = "..\\bulid\\megax\\aligned\\"
     start = time.time()
     threads = []
     thrNum = len(list_name(readPath))
-    threadmax = threading.BoundedSemaphore(8)  # 限制线程的最大数量为4个
+    threadmax = threading.BoundedSemaphore(16)  # 限制线程的最大数量为4个
     lock = threading.Lock()  # 将锁内的代码串行化
     for i in range(thrNum):
         threadmax.acquire()  # 增加信号量，可用信号量减一
@@ -34,4 +34,4 @@ if __name__ == '__main__':
     for t in threads:
         t.join()
     end = time.time()
-    print("执行用时{}".format(end - start))
+    print("已完成，执行用时{:.2f}秒。".format(end - start))
